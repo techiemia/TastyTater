13bdd3fdbd3950c476514ce283e2165ef34031bf
techie_mia <jahmiaherbert88@gmail.com>
2023-10-25 09:56:45 -0500

    updated api for items and review

--- backend/TastyTater/urls.py
+++ backend/TastyTater/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-
+from items.views import items_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('items.urls')),
     # re_path('', TemplateView.as_view(template_name='index.html')),
+    path('', items_view, name='items'),
 ]
--- backend/items/views.py
+++ backend/items/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 from .models import Items
 from .serializers import ItemsSerializer
+from django.http import JsonResponse
+
 
 class ItemsList(ListAPIView):
     queryset = Items.objects.filter(parent__isnull=True)
     serializer_class = ItemsSerializer
+
+
+def items_view(request):
+   data = [{'American Food': 10.99 }, {'Cheesy Prawns with corn': 10.99}, {'Cheese Burst Roll': 10.99}, {'Greeny Snails': 10.99}, {'Spicy Momos': 10.99}, {'Chicken Broast': 10.99}]
+
+   return JsonResponse(data, safe=False)
